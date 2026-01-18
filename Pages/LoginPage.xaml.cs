9c564cf6ee5a65fbb65476b85be6dbf9bc580305
unknown <Pavelvankov@gmail.com>
2026-01-18 23:06:18 +0500

    first commit

---
+++ Pages/LoginPage.xaml.cs
@@ -0,0 +1,76 @@
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+namespace PriemPatient.Pages
+{
+    public partial class LoginPage : Page
+    {
+        public User CurrentUser { get; set; } = new User();
+
+        public LoginPage()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void LogInButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(LogId.Text) || string.IsNullOrWhiteSpace(LogPass.Text))
+            {
+                MessageBox.Show("Все поля обязательны для заполнения");
+                return;
+            }
+
+            if (!int.TryParse(LogId.Text, out int userId))
+            {
+                MessageBox.Show("ID должен быть числом");
+                return;
+            }
+
+            string filePath = GetUser(userId);
+            if (!File.Exists(filePath))
+            {
+                MessageBox.Show("Пользователь с таким ID не найден");
+                return;
+            }
+
+            try
+            {
+                string json = File.ReadAllText(filePath);
+                var user = JsonConvert.DeserializeObject<User>(json);
+                if (user != null && user.Password == LogPass.Text)
+                {
+                    CurrentUser.Id = user.Id;
+                    CurrentUser.Name = user.Name;
+                    CurrentUser.LastName = user.LastName;
+                    CurrentUser.MiddleName = user.MiddleName;
+                    CurrentUser.Specialisation = user.Specialisation;
+                    CurrentUser.Password = user.Password;
+                    NavigationService.Navigate(new MainPage(user));
+                }
+                else
+                {
+                    MessageBox.Show("Неверный пароль");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка при загрузке пользователя");
+            }
+        }
+
+        private void RegButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new RegistrationPage());
+        }
+
+        private string GetUser(int userID)
+        {
+            return $"D_{userID}.json";
+        }
+    }
+}
---
+++ Patient.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Runtime.CompilerServices;
+
+namespace PriemPatient
+{
+    public class Patient : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        private int _idP = 0;
+        public int IdP
+        {
+            get => _idP;
+            set
+            {
+                if (_idP != value)
+                {
+                    _idP = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _nameP = "";
+        public string NameP
+        {
+            get => _nameP;
+            set
+            {
+                if (_nameP != value)
+                {
+                    _nameP = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _lastnameP = "";
+        public string LastNameP
+        {
+            get => _lastnameP;
+            set
+            {
+                if (_lastnameP != value)
+                {
+                    _lastnameP = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _middlenameP = "";
+        public string MiddleNameP
+        {
+            get => _middlenameP;
+            set
+            {
+                if (_middlenameP != value)
+                {
+                    _middlenameP = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _lastAppointment = "";
+        public string LastAppointment
+        {
+            get => _lastAppointment;
+            set
+            {
+                if (_lastAppointment != value)
+                {
+                    _lastAppointment = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _birthday = "";
+        public string Birthday
+        {
+            get => _birthday;
+            set
+            {
+                if (_birthday != value)
+                {
+                    _birthday = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _diagnosisP = "";
+        public string DiagnosisP
+        {
+            get => _diagnosisP;
+            set
+            {
+                if (_diagnosisP != value)
+                {
+                    _diagnosisP = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _recomendationsP = "";
+        public string RecomendationsP
+        {
+            get => _recomendationsP;
+            set
+            {
+                if (_recomendationsP != value)
+                {
+                    _recomendationsP = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        private string _phoneNumber = "";
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set { _phoneNumber = value; OnPropertyChanged(); }
+        }
+
+        private Appointment[] _appointmentStories = new Appointment[0];
+        public Appointment[] AppointmentStories
+        {
+            get => _appointmentStories;
+            set
+            { 
+                _appointmentStories = value; 
+                OnPropertyChanged(); 
+            }
+        }
+    }
+
+    public class Appointment
+    {
+        public string Date { get; set; } = "";
+
+        public int DoctorId { get; set; }
+
+        public string Diagnosis { get; set; } = "";
+        public string Recomendations { get; set; } = "";
+    }
+}
